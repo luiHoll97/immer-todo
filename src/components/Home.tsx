e72c2ee40595bc552359827ba3c662b5146ef2a4
luiHoll97 <tech.luiholl@gmail.com>
2023-08-18 21:47:49 +0100

    basic tab layout for todos/archives

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,24 @@
 import Todo from "./Todo";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
+
 
 const Home = () => {
     return (
         <>
-            <Todo />
+            <Tabs isFitted variant='enclosed'>
+                <TabList mb='1em'>
+                    <Tab>Active</Tab>
+                    <Tab>Archive</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <Todo />
+                    </TabPanel>
+                    <TabPanel>
+                        <p>two!</p>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
         </>
     )
 }
